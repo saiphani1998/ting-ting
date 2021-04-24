c18ab967ff664a94752f8c80ad33e9d82cbcffc4
saiphani1998 <saiphani@gamenous.com>
2021-04-24 19:36:57 +0530

    Deploy to Heroku

---
+++ index.js
@@ -0,0 +1,43 @@
+const express = require("express");
+const app = express();
+const server = require("http").createServer(app);
+const cors = require("cors");
+const path = require("path");
+
+const io = require("socket.io")(server, {
+  cors: {
+    origin: "*",
+    methods: ["Get", "POST"],
+  },
+});
+
+app.use(cors());
+
+const PORT = process.env.PORT || 5000;
+
+app.use(express.static(__dirname + "/app/dist/build"));
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/app/build/index.html");
+});
+
+app.get("/*", (req, res) => {
+  res.sendFile(path.join(__dirname + `/app/build/${req.originalUrl}`));
+});
+
+io.on("connection", (socket) => {
+  socket.emit("me", socket.id);
+  socket.on("disconnect", () => {
+    socket.broadcast.emit("Call ended");
+  });
+
+  socket.on("calluser", ({ userToCall, signalData, from, name }) => {
+    io.to(userToCall).emit("calluser", { signal: signalData, from, name });
+  });
+
+  socket.on("answercall", (data) => {
+    io.to(data.to).emit("callaccepted", data.signal);
+  });
+});
+
+server.listen(PORT, () => console.log(`Server is listening on ${PORT}`));
