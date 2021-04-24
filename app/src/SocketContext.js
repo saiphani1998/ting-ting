e1dff44df3af171e0a3f79eca346046151bd06f8
saiphani1998 <saiphani@gamenous.com>
2021-04-24 20:05:59 +0530

    deleted redundant code

--- app/src/SocketContext.js
+++ app/src/SocketContext.js
@@ -30,15 +30,6 @@ const ContextProvider = ({ children }) => {
       })
       .catch((error) => {
         console.log("vvgv");
-        navigator.mediaDevices
-          .getUserMedia({
-            video: true,
-            audio: true,
-          })
-          .then((currentStream) => {
-            setStream(currentStream);
-            myVideo.current.srcObject = currentStream;
-          });
       });
 
     socket.on("me", (id) => {
