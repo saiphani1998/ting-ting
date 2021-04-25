980299ca83b4e6daa8b246334fdf3c5f80d54798
saiphani1998 <saiphani@gamenous.com>
2021-04-25 22:28:45 +0530

    Added Dial Tone for outgoing call

--- app/src/components/Notifications.jsx
+++ app/src/components/Notifications.jsx
@@ -1,20 +1,24 @@
 import React, { useContext } from "react";
 import { Button } from "@material-ui/core";
-import ring_ring from "../assets/ring_ring.mp3";
+import ringTone from "../assets/ring_ring.mp3";
+import dialTone from "../assets/dial_tone.mp3";
 
 import { SocketContext } from "../SocketContext";
 
 export default function Notifications() {
-  const { answerCall, call, callAccepted } = useContext(SocketContext);
+  const { answerCall, call, callAccepted, callOutgoing } = useContext(
+    SocketContext
+  );
   return (
     <>
+      {callOutgoing ? <audio src={dialTone} autoPlay loop></audio> : <></>}
       {call.isReceivingCall && !callAccepted && (
         <div style={{ display: "flex", justifyContent: "center" }}>
           <h1>{call.name} is Calling</h1>
           <Button variant="contained" color="primary" onClick={answerCall}>
             Answer
           </Button>
-          <audio id="caller-tune" src={ring_ring} autoPlay loop></audio>
+          <audio src={ringTone} autoPlay loop></audio>
         </div>
       )}
     </>
