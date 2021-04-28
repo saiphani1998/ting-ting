fd61ab762ebd9e925c9e7b3802074969c7973319
saiphani1998 <saiphani@gamenous.com>
2021-04-29 00:25:05 +0530

    Reduced Ringtone and dialtone volume

--- app/src/components/Notifications.jsx
+++ app/src/components/Notifications.jsx
@@ -9,18 +9,43 @@ export default function Notifications() {
   const { answerCall, call, callAccepted, callOutgoing } = useContext(
     SocketContext
   );
+
+  const setVolume = (element, volume) => {
+    element.volume = volume;
+  };
+
+  const ringtoneVolumeSetter = () => {
+    const element = document.getElementById("ringTone");
+    console.log(element);
+    setVolume.apply(this, [element, 0.05]);
+  };
+
+  const dialToneVolumeSetter = () => {
+    const element = document.getElementById("dialTone");
+    console.log(element);
+    setVolume.apply(this, [element, 0.2]);
+  };
+
   return (
     <>
-      {callOutgoing ? <audio src={dialTone} autoPlay loop></audio> : <></>}
-      {call.isReceivingCall && !callAccepted && (
-        <div style={{ display: "flex", justifyContent: "center" }}>
-          <h1>{call.name} is Calling</h1>
-          <Button variant="contained" color="primary" onClick={answerCall}>
-            Answer
-          </Button>
-          <audio src={ringTone} autoPlay loop></audio>
-        </div>
+      {callOutgoing ? (
+        setTimeout(() => dialToneVolumeSetter(), 0) && (
+          <audio id="dialTone" src={dialTone} autoPlay loop></audio>
+        )
+      ) : (
+        <></>
       )}
+      {call.isReceivingCall &&
+        !callAccepted &&
+        setTimeout(() => ringtoneVolumeSetter(), 0) && (
+          <div style={{ display: "flex", justifyContent: "center" }}>
+            <h1>{call.name} is Calling</h1>
+            <Button variant="contained" color="primary" onClick={answerCall}>
+              Answer
+            </Button>
+            <audio id="ringTone" src={ringTone} autoPlay loop></audio>
+          </div>
+        )}
     </>
   );
 }
